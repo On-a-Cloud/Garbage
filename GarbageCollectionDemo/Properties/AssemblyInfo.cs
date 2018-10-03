165e55297ad05452b9b8f7152929a9f1287687bd
Humberto Banuelos <humberto.banuelos13@outlook.com>
2018-10-03 12:58:14 -0400

    Add project files.

---
+++ GarbageCollectionDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GarbageCollectionDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GarbageCollectionDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+[assembly: Guid("50d3ec32-9581-47a5-bbf4-333f0235551b")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
